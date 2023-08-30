da522d68dec62d3f986dc538d551bd9e158b3f27
Jonathan O'Leary <jonathanoleary24@gmail.com>
2023-08-30 02:26:06 +0100

    User function added (no specific stations yet)

--- routes.js
+++ routes.js
@@ -1,14 +1,21 @@
 import express from "express";
+
+import { accountsController } from "./controllers/accounts-controller.js";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { stationController } from "./controllers/station-controller.js";
 
-
 export const router = express.Router();
 
-router.get("/", dashboardController.index);
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
+
 router.get("/dashboard", dashboardController.index);
 router.post("/dashboard/addstation", dashboardController.addStation);
-router.get("/about", aboutController.index);
 router.get("/station/:id", stationController.index);
 router.post("/station/:id/addReading", stationController.addReading);
+router.get("/about", aboutController.index);
